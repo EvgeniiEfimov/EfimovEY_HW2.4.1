968740f89b75b180bc539a418e8fe361222b0120
1.0.0 <1.0.0>
2021-07-13 13:18:11 +0300

    finish view

---
+++ EfimovEY_HW2.3/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  EfimovEY_HW2.3
+//
+//  Created by User on 13.07.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
